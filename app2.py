deab98ab870b776f78f3f4535c939efc21479f3d
ahuang20stuy <ahuang20@stuy.edu>
2018-11-26 12:36:03 -0500

    login

--- app2.py
+++ app2.py
@@ -41,6 +41,29 @@ def authPage():
 def reg():
     return render_template('reg.html')
 
+@app.route("/added",methods=['GET','POST'])
+def added():
+    DB_FILE="data/quackamoo.db"
+    newUsername = request.form['username']
+    newPassword = request.form['password']
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor() 
+    command = 'SELECT username FROM users;'
+    c.execute(command)
+    userList = c.fetchall()
+    print(userList)
+    if newUsername not in userList:
+        insert = "INSERT INTO users VALUES(?,?)"
+        params=(newUsername,newPassword)
+        c.execute(insert,params)
+        db.commit()
+        db.close()
+        #session['username'] = newUsername
+        return redirect(url_for('home'))
+    else:
+        flash('Username Taken')
+        return redirect(url_for('home')
+
 
 
 if __name__ == '__main__':

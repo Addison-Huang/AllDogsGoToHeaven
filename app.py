73db622b09f224c0ebe899d29f96bf62e1f5053e
DenChen11214 <dchen22@stuy.edu>
2018-11-28 12:04:17 -0500

    worked on getting list of questions worth certain numbers of points

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from util import db_updater as update
 from flask import Flask,render_template,request,session,url_for,redirect,flash
 from os import urandom
+import urllib.request
+import json
 
 import sqlite3 #imports sqlite
 DB_FILE="data/quackamoo.db"
@@ -67,25 +69,15 @@ def added():
 @app.route('/categories')
 def startGame():
     url = "http://jservice.io/api/clues?value="
-    url100 = urllib.request.urlopen(url + "100")
-    url200 = urllib.request.urlopen(url + "200")
-    url300 = urllib.request.urlopen(url + "300")
-    url400 = urllib.request.urlopen(url + "400")
-    url500 = urllib.request.urlopen(url + "500")
-    url600 = urllib.request.urlopen(url + "600")
-    url700 = urllib.request.urlopen(url + "700")
-    url800 = urllib.request.urlopen(url + "800")
-    url900 = urllib.request.urlopen(url + "900")
-    url1000 = urllib.request.urlopen(url + "1000")
-    data = json.loads(url100.read())
-    data = json.loads(url100.read())
-    data = json.loads(url100.read())
-    data = json.loads(url100.read())
-    data = json.loads(url100.read())
-    data = json.loads(url100.read())
-    data = json.loads(url100.read())
-    data = json.loads(url100.read())
-    print(data)
+    urlList = []
+    dataList = []
+    for i in range(1,11):
+        urlList.append(urllib.request.urlopen(url + str(i * 100)))
+        dataList.append(json.loads(urlList[i-1].read()))
+    del dataList[6]
+    del dataList[8]
+    for i in range(7):
+        print(dataList[i][0]['question'])
     return render_template('categories.html')
 
 if __name__ == '__main__':

5c2baf9c30a79f40ddc360f0a97e984b7d00b7f7
ahuang20stuy <ahuang20@stuy.edu>
2018-11-30 12:32:54 -0500

    db_updater code

--- util/db_updater.py
+++ util/db_updater.py
@@ -26,7 +26,7 @@ score is the point value of the question added
 function adds the point value of the question to the users score
 '''
 def addScore(username,score):
-    command = "SELECT score FROM users WHERE users.username ='" + title + "';" #selects score of the user
+    command = "SELECT score FROM users WHERE users.username ='" + username + "';" #selects score of the user
     c.execute(command)
     oldScore = c.fetchall()
     newScore = oldScore + score 
@@ -42,6 +42,25 @@ function subtracts the point value of the question from the users score
 '''
 def subScore(username, score):
     addScore(username, score * - 1)
+
+'''
+ansQuestion(username, question, answer)
+params:username, question, answer
+username is the username of the user in session
+question is the question the user answered
+answer is api provided answer for the question
+'''
+
+def ansQuestion(username, question, answer):
+    insert = "INSERT INTO questions VALUES(?,?,?)"
+    params = (username, question,username)
+    c.execute(insert, params)
+    
+    
+    
+    
+
+
 
 
     
